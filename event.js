3ac299cab8c4de3e8b2a7054cc60299fb29abc26
sachi1010 <131956987+sachi1010@users.noreply.github.com>
2025-02-23 16:53:27 +0530

    Add files via upload

---
+++ event.js
@@ -0,0 +1,55 @@
+
+
+// function Something(){
+//     alert("this is a function");
+// }
+// h1.addEventListener("click",Something);
+// h1.removeEventListener("click",Something)
+
+
+// h1.addEventListener("click",(event)=>
+// {
+//     console.log(event.target);
+//     event.target.textContent="this is a new text";
+// }
+// );
+
+document.addEventListener("readystatechange",(event)=>{
+    if(event.target.readyState==="complete")
+        {
+            
+            console.log("complete");
+            afterpage();
+        }
+});
+
+const afterpage=()=>{
+   const page=document.querySelector("#content");
+   const div=page.querySelector("div");
+   const h1=div.querySelector("h1")
+
+   page.addEventListener("click",(event)=>{
+             page.classList.toggle("red");
+            page.classList.toggle("darkblue");
+});
+
+div.addEventListener("mouseover",(event)=>{
+    event.stopPropagation(); 
+    div.style.backgroundColor="red";
+    div.style.color="white";
+    div.style.padding="20px";
+    div.style.margin="20px";
+    div.style.border="1px solid black";
+    div.style.width="50%";
+    div.style.transition="background-color 2s ease-out";
+})
+
+h1.addEventListener("click",(event)=>{
+    event.stopPropagation(); 
+
+    const myText=event.target.textContent;
+    myText==="main content"?(event.target.textContent="python"):(event.target.textContent="main content");
+})
+
+
+}  
