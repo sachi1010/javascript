883c53d1e5d57e5bc4613b501daa3fc661c64c7f
sachi1010 <sachisachi8567@gmail.com>
2025-02-25 12:07:12 +0530

    login

--- animatedform.js
+++ animatedform.js
@@ -1,21 +1,35 @@
-function login(){
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    let email = document.getElementById('email').value;
-        let data = {
-            "username": username,
-            "password": password,
-            "email": email
+// function login(){
+//     let username = document.getElementById('username').value;
+//     let password = document.getElementById('password').value;
+//     let email = document.getElementById('email').value;
+//         let data = {
+//             "username": username,
+//             "password": password,
+//             "email": email
 
-        };
-        console.log(data);
-}
-function signup(){
-    const form=document.querySelector("#login");
-    form.addEventListener("click",(event)=>{
-        event.preventDefault();
+//         };
+//         console.log(data);
+// }
+// function signup(){
+//     const form=document.querySelector("#login");
+//     form.addEventListener("click",(event)=>{
+//         event.preventDefault();
     
        
 
-    })
-}
+//     })
+// }
+
+
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
