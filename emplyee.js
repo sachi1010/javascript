949af220bee086d70c9de05aade08ceacce99abf
sachi1010 <sachisachi8567@gmail.com>
2025-03-07 20:02:43 +0530

    Update emplyee.js

--- emplyee.js
+++ emplyee.js
@@ -1,60 +1,62 @@
-class Employeemanage{
+
+  class Employeemanage{
     #name;
-    #emp_id;
     #age;
-    #workcategory;
-
-    constructor(name,empid,age,workcategory){
-        this.#name=name;
-        this.#emp_id=empid;
-        this.#age=age;
-        this.#workcategory=workcategory;
+    #salary;
+  
+    constructor(name, age, salary) {
+      this.#name = name;
+      this.#age = age;
+      this.#salary = salary;
     }
 
-}
+    getName() {
+        return this.#name;
+      }
+    
+      setName(name) {
+        this.#name = name;
+      }
+    
+      getAge() {
+        return this.#age;
+      }
+    
+      setAge(age) {
+        this.#age = age;
+      }
+    
+      getSalary() {
+        return this.#salary;
+      }
 
-class name{
-    constructor(name){
-        this.name;
-           return name;
-    }
-    display(){
-        console.log(`employee name is ${this.name}`)
-    }
-}
 
-class employeeid{
-    constructor(empid){
-        this.empid;
-        return empid;
-    }
-    id(){
-        console.log(`employee id is ${this.empid}`)
-    }
-}
+      setSalary(salary) {
+        this.#salary = salary;
+      }
 
-class age{
-    constructor(age){
-        this.age;
-        return age;
-    }
-Age(){
-        console.log(`employee age is ${this.age}`)
-    }
-}
+      displayInfo() {
+        console.log(`employee name is ${this.#name} Age: ${this.#age} Salary: ${this.#salary}`);
+      }
+  }
 
-class workcategory{
-    constructor(workcategory){
-        this.workcategory;
-        return workcategory;
+  class Manager extends Employee {
+    #department;
+  
+    constructor(name, age, salary, department) {
+      super(name, age, salary);
+      this.#department = department;
     }
-    category(){
-        console.log(`your category is ${this.workcategory}`)
-    }
-}
+  
+    getDepartment() {
+        return this.#department;
+      }
 
-const emp1=new Employeemanage("aravind",23,154,"manager");
-emp1.display();
-emp1.id();
-emp1.Age();
-emp1.category();
+      setDepartment(department) {
+        this.#department = department;
+      }
+      displayInfo() {
+        super.displayInfo();
+        console.log(`Department: ${this.#department}`);
+      }
+    }
